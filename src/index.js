cbb9adb1663d441109ef859ddaa4d4bd9c122926
Iheanacho Victor Nonso <victor@Iheanachos-MacBook-Pro.local>
2020-03-02 10:37:56 +0100

    setup express server #1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ResHelpers } = require("./utils");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => ResHelpers.res(res, "Welcome !!"));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
